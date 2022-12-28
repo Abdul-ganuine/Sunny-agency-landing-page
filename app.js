0f409b83b3e98c8afec7f5ce6980ce59383be6a3
Abdul-ganuine <118227746+Abdul-ganuine@users.noreply.github.com>
2022-12-28 12:51:29 +0000

    Add files via upload
    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let humBurgerMenu=document.querySelector('.menu');
+let navShow=document.querySelector('#nav');
+
+humBurgerMenu.addEventListener('click',function(){
+    navShow.classList.toggle('active');
+})
+
+
